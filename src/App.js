5ca2fe46347338ecb5999ff7bd324f11ececbeb9
ShadePrima <aleksandrag1984@gmail.com>
2023-04-17 12:57:19 +0300

    user event

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const inputStyle = {
+  marginTop: '10px',
+}
+
 function App() {
+  const [data, setData] = useState(null)
+  const [toggle, setToggle] = useState(false)
+  const [value, setValue] = useState('')
+
+  const onClick = () => setToggle((prev) => !prev)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({})
+    }, 1000)
+  }, [])
+
   return (
     <div className='App'>
+      <div data-testId='value-elem'>{value}</div>
+      {data && <div style={{ color: 'red' }}>data</div>}
       <h1>Hello world</h1>
+      <button data-testid='toggle-btn' onClick={onClick}>
+        Click me
+      </button>
+      <br />
+      <input
+        style={inputStyle}
+        type='text'
+        name='text'
+        id='text'
+        placeholder='input value'
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {toggle === true && <div data-testid='toggle-element'>toggle</div>}
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test app', () => {
+  test('renders learn react link', () => {
+    render(<App />)
+    const helloWorldElement = screen.getByText(/hello world/i)
+    const button = screen.getByRole('button')
+    const input = screen.getByPlaceholderText(/input value/i)
+    expect(helloWorldElement).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(input).toMatchSnapshot()
+  })
+
+  test('renders learn react element', async () => {
+    render(<App />)
+    // const helloWorldElement = screen.queryByText(/hele world 2/i)
+    // expect(helloWorldElement).toBeNull()
+
+    screen.debug()
+    const helloWorldElement = await screen.findByText(/data/i)
+    expect(helloWorldElement).toBeInTheDocument()
+    expect(helloWorldElement).toHaveStyle({ color: 'red' })
+    screen.debug()
+  })
+
+  test('event button', () => {
+    render(<App />)
+    const button = screen.getByTestId('toggle-btn')
+    const toggleDiv = screen.queryByTestId('toggle-element')
+    expect(toggleDiv).toBe(null)
+    fireEvent.click(button)
+    expect(screen.getByTestId('toggle-element')).toBeInTheDocument()
+    fireEvent.click(button)
+    expect(screen.queryByTestId('toggle-element')).toBeNull()
+  })
+
+  test('event input', () => {
+    render(<App />)
+    const input = screen.getByPlaceholderText(/input value/i)
+    expect(screen.queryByTestId('value-elem')).toContainHTML('')
+    //artificalt even
+    // fireEvent.input(input, {
+    //   target: {
+    //     value: '123123',
+    //   },
+    // })
+    //user event
+    userEvent.type(input, '123123')
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123123')
+  })
+})
